8077a0f245851895dbccd4326434ec8e6c1b51dc
kopisopi <sophiekim455@gmail.com>
2026-05-16 22:48:53 -0700

    Update testfoo.py

--- tests/testfoo.py
+++ tests/testfoo.py
@@ -19,8 +19,30 @@ class TestUserPassword:
         assert UserProfile.valid_password("Secure123!") == True
         assert UserProfile.valid_password("Secure123!!") == True
         assert UserProfile.valid_password("S123ecure!") == True
+
+        assert UserProfile.valid_password("SECURE123!") == False
+        assert UserProfile.valid_password("secure123!") == False
+        assert UserProfile.valid_password("Secureabc!") == False
+        assert UserProfile.valid_password("Secure123") == False
+
+        assert UserProfile.valid_password("Ab1!AAAA") == True
+
+        assert UserProfile.valid_password("A1!bcdefg") == True
+        assert UserProfile.valid_password("abcdefA1!") == True
+        assert UserProfile.valid_password("abcA1!def") == True
+        assert UserProfile.valid_password("Aaaaa11!") == True
+
+
+        assert UserProfile.valid_password(" Secure123!") == False
+        assert UserProfile.valid_password("Secure 123") == False
+        assert UserProfile.valid_password("Secure12\n!") == False
+        assert UserProfile.valid_password("Secure12!\n") == False
+        assert UserProfile.valid_password("Secure12\t!") == False
+
+        assert UserProfile.valid_password("Secure12\t!") == False
         
-        
+
+
         # the following below should be true but doesn't apss the test
         assert UserProfile.valid_password("!Password1") == True
         assert UserProfile.valid_password("1Password!") == True
@@ -31,6 +53,8 @@ class TestUserPassword:
         assert UserProfile.valid_password("!!1Password234") == True
         assert UserProfile.valid_password("!!*****1Pd234") == True
         assert UserProfile.valid_password("p111111****jsdfsdfajjasdasodjasdaP") == True
+
+        
         
 
 
